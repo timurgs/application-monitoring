4b8df0224240daa6b197be986eb0caac899d588f
Timur Galiev <timurgravirovsky@gmail.com>
2022-10-30 18:56:22 +0300

    [+] Created the project structure

---
+++ backend/settings_local.py
@@ -0,0 +1,14 @@
+import os
+
+SECRET_KEY = os.getenv('SECRET_KEY', '')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', ''),
+        'USER': os.getenv('POSTGRES_USER', ''),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('HOST', '127.0.0.1'),
+        'PORT': os.getenv('PORT', '5432'),
+    }
+}
