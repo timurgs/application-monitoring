52a8f6b5e261dc8f6a9a9bea4ae7eeb387179973
Timur Galiev <timurgravirovsky@gmail.com>
2022-11-06 20:42:48 +0300

    [-] Django-Toolbar

--- application_monitoring/settings.py
+++ application_monitoring/settings.py
@@ -40,8 +40,6 @@ INSTALLED_APPS = [
     'backend',
     'rest_framework',
     'rest_framework.authtoken',
-
-    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -52,8 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'application_monitoring.urls'
